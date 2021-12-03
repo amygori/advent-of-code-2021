12bd247398449ed576b59597593b3fe27034ef6c
Amy Gori <amydgori@gmail.com>
2021-12-02 21:56:19 -0500

    Day 2 part 1

---
+++ day2/d2.1.py
@@ -0,0 +1,37 @@
+import sys
+from pathlib import Path
+
+
+class Submarine:
+    def __init__(self):
+        self.horizontal_position = 0
+        self.depth = 0
+
+    def down(self, increment):
+        self.depth += increment
+
+    def up(self, increment):
+        self.depth -= increment
+
+    def forward(self, increment):
+        self.horizontal_position += increment
+
+
+def pilot_the_sub(steps, submarine):
+    for step in steps:
+        direction, increment = step.split()
+        go = getattr(submarine, direction)
+        go(int(increment))
+
+    print(submarine.depth * submarine.horizontal_position)
+
+
+if __name__ == "__main__":
+    file = Path(sys.argv[1])
+    if Path.is_file(file):
+        with Path(file).open() as f:
+            input = f.read().splitlines()
+            submarine = Submarine()
+            pilot_the_sub(input, submarine)
+    else:
+        raise TypeError("This is not a file")
