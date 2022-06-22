6aff8e87b3343034803a59d34a5a8b78bbbc213f
Amy Gori <amydgori@gmail.com>
2022-06-21 22:35:36 -0400

    Day 9 part 2

--- day9/day9.2.py
+++ day9/day9.2.py
@@ -2,25 +2,17 @@ import sys
 from pathlib import Path
 
 
-# track points that have already been counted
 visited_points = []
 
 
 def print_results(input):
-    print("🥑 🍉 🥒 🍒 🥑 🍉 🥒 🍒 HERE WE GO 🥑 🍉 🥒 🍒 🥑 🍉 🥒 🍒")
     basins = []
-    lowest_points_list = lowest_points(input)
-    short_list = [(0, 1), (0, 9)]
 
-    print(lowest_points_list)
-    # [(0, 1), (0, 9), (2, 2), (4, 6)]
     for point in lowest_points(input):
         basin_size = check_points(point, input)
-        print(f"🌳 YO we got a return value for point and it is {basin_size} 🌳")
-        print(f"🎯 basin size for point {point} is {basin_size}")
-        print(f"Visited points are {visited_points}")
         basins.append(basin_size)
-    print(f"🌊 {basins}")
+    b1, b2, b3 = sorted(basins)[-3:]
+    print(f"Multiplying the size of the three largest basins: {b1 * b2 * b3}")
 
 
 def num_at_point(point, input):
@@ -29,36 +21,22 @@ def num_at_point(point, input):
 
 
 def check_points(point, input, count=0):
-    # first time we check the low point, then we will need to check each of its neighbors.
-    print("🦙 🦙 🦙 Count points! inputs are:")
-    print(f"point: {point} | count: {count}")
-
     line_index, num_index = point
     line = input[line_index]
-    if point in visited_points:
-        return count
 
-    # increment counter if not a 9 (we're still in the basin) and it's not already counted
-    # if num_at_point(point, input) < 9 and point not in visited_points:
+    # mark that we've checked this point
     visited_points.append(point)
-    print(f"🦩 Added to Visited points: {visited_points}")
 
+    # increment the counter
     count += 1
 
     # find its neightbors
     neighbors = get_neighbors(point, input)
-    print(f"👽 Neighbors for {point} are {neighbors} 👽")
 
+    # recursively call this function for each neighboring point that should be checked
     for neighbor in neighbors:
-        print("🍥 about to check neighbor ", neighbor)
         if num_at_point(neighbor, input) < 9 and neighbor not in visited_points:
-            print(f"🧞‍♂️ Neighbor {neighbor} is about to be checked 🧞‍♂️")
-            print(f"🫐 Count is currently {count} 🫐")
             count = check_points(neighbor, input, count)
-        else:
-            print(
-                f"Neighbor {neighbor} did not pass test because it is {num_at_point(neighbor, input)} or in visited points: {visited_points}"
-            )
 
     return count
 
@@ -82,85 +60,6 @@ def get_neighbors(point, input):
         neighbors.append(up)
 
     return neighbors
-
-
-def count_x(point, input):
-    line_idx, num_idx = point
-    line = input[line_idx]
-
-    right = count_right(line, line_idx, num_idx)
-    left = count_left(line, line_idx, num_idx)
-    print(f"❌ In count_x, right count is {right}")
-    print(f"🍋 and left count is {left}")
-
-    return right + left
-
-
-def count_y(point, input):
-    line_idx, num_idx = point
-
-    up = count_up(line_idx, num_idx, input)
-    down = count_down(line_idx, num_idx, input)
-    print(f"In count_y, up count is {up} and down count is {down}")
-    return up + down
-
-
-def count_right(line, line_idx, num_idx, count=0):
-    print("🪴 count right starts here 🪐")
-    # check to make sure that it's not at the right edge
-    if num_idx < (len(line) - 1):
-        print(f"The number we are considering is {line[num_idx]}")
-        current_point = (line_idx, num_idx)
-        if current_point not in visited_points and int(line[num_idx]) != 9:
-            print(f"🧁 current point {current_point} not in visited points; counting 🍵")
-            count = 1
-            # visited_points.append(current_point)
-            # count_right(line, line_idx, num_idx + 1, count)
-
-    return count or 0
-
-
-def count_left(line, line_idx, num_idx, count=0):
-    # check to make sure it's not at the left edge
-    print("🍋 🍃 Count Left is called 🍋 🍃")
-    if num_idx > 0:
-        print(f"The number we are considering is {line[num_idx]}")
-        current_point = (line_idx, num_idx)
-        if current_point not in visited_points and int(line[num_idx]) != 9:
-            print(f"🧁 current point {current_point} not in visited points; counting 🍵")
-            count = 1
-            # visited_points.append(current_point)
-        # count_left(line, line_idx, num_idx - 1, count)
-    return count or 0
-
-
-def count_down(line_idx, num_idx, input, count=0):
-    # check to make sure it's not at the bottom edge
-    if line_idx < (len(input) - 1):
-        line = input[line_idx]
-        current_point = (line_idx, num_idx)
-        print("In count down, looking at ", input[line_idx][num_idx])
-        if current_point not in visited_points and int(line[num_idx]) != 9:
-            print("🐙 Hit a 9 in count down")
-            count = 1
-            # visited_points.append(current_point)
-
-        # count_down(line_idx + 1, num_idx, input, count)
-    return count or 0
-
-
-def count_up(line_idx, num_idx, input, count=0):
-    # check to make sure it's not at the top edge
-    if line_idx > 0:
-        line = input[line_idx]
-        current_point = (line_idx, num_idx)
-        print("In count up, looking at ", input[line_idx][num_idx])
-
-        if current_point not in visited_points and int(line[num_idx]) != 9:
-            count = 1
-            # visited_points.append(current_point)
-        # count_up(next_line, num_idx, input, count)
-    return count or 0
 
 
 def lowest_points(input):
